ca2d25c544c2c9ff412ecc1379b30fef7017640f
roman <skyfoxwork@gmail.com>
2024-12-12 16:00:56 +0200

    Solution-py-tickets-orders v1.2

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "debug_toolbar",
+
     "cinema",
     "user",
 ]
